7c98d6b02cf0f0489cbcf09c110f28bd9aa5bab7
Robin Lane <rubeculabee@gmail.com>
2025-11-10 16:37:01 -0800

    implemented loading the dictionary

--- src/StandardEdition/StdDictionary.java
+++ src/StandardEdition/StdDictionary.java
@@ -1,9 +1,20 @@
 package StandardEdition;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
 class StdDictionary
 {
 	public static void main(String[] args)
 	{
-		System.out.println(StdEntry.BOOK_NOUN.getDefinition());
+		Multimap<String, String> dictionary = loadDict();
 	}
+
+	private static Multimap<String, String> loadDict()
+	{
+		Multimap<String, String> dict = ArrayListMultimap.create();	
+		for(StdEntry entry : StdEntry.values())
+			dict.put(entry.getWord(), entry.getDefinition());
+		return dict;
+	} 
 }
