578768152071c0c4604584091da2c88f22b081bf
Robin Lane <rubeculabee@gmail.com>
2025-11-13 12:45:58 -0800

    Implented ProEntry. Enums contain all the words definitions and each part of speech those definitions describe

--- src/ProfessionalEdition/ProEntry.java
+++ src/ProfessionalEdition/ProEntry.java
@@ -2,24 +2,41 @@ package ProfessionalEdition;
 
 enum ProEntry
 {
-	BOOK_NOUN("Book", "A Written work published in printed or electronic form."),
-	BOOK_VERB("Book", "To arrange for someone to have a seat on a plane."),
-	BOOKABLE("Bookable", "Can be ordered in advance."),
-	BOOKCASE("Bookcase", "A piece of furniture with shelves."),
-	BOOKBINDER("Bookbinder", "A person who fastens the pages of books."),
-	CSC220_NOUN("CSC220", "Data Structures."),
-	CSC220_ADJ("CSC220", "Ready to create complex data structures."),
-	CSC220_VERB("CSC220", "To create data structures.");
+	Book(
+		new String[] {"A Written work published in printed or electronic form.", "To arrange for someone to have a seat on a plane."},
+		Speech.NOUN, Speech.VERB
+	),
+	Bookable(
+		new String[] {"Can be ordered in advance."},
+		Speech.ADJECTIVE
+	),
+	Bookcase(
+		new String[] {"A piece of furniture with shelves."},
+		Speech.NOUN
+	),
+	Bookbinder(
+		new String[] {"A person who fastens the pages of books."},
+		Speech.NOUN
+	),
+	CSC220(
+		new String[] {"Ready to create complex data structures.", "Data Structures", "To create data structures."},
+		Speech.ADJECTIVE, Speech.NOUN, Speech.VERB
+	);
 
-	private String word;
-	private String definition;
+	private String[] definitions;
+	private Speech[] speechTypeOrder;
 
-	private ProEntry(String word, String definition)
+	private ProEntry(String[] definitions,Speech... typeOrder)
 	{
-		this.word = word;
-		this.definition = definition;
+		this.definitions = definitions;
+		this.speechTypeOrder = typeOrder;
 	}
 
-	public String getWord() {return this.word;}
-	public String getDefinition() {return this.definition;}
+	public String getDefinition(int index) {return this.definitions[index];}
+	public Speech getSpeechType(int index) {return this.speechTypeOrder[index];}
+
+	private enum Speech
+	{
+		NOUN, VERB, ADJECTIVE, ADVERB;
+	}
 }
